519b817d68470fe40f775ab4d969cb70e1ab49e2
Marc Baumgartner <marc@mabaka.ch>
2020-04-24 21:29:04 +0200

    Simple test service.

--- src/main/java/ch/mabaka/journal/app/GreetingResource.java
+++ src/main/java/ch/mabaka/journal/app/GreetingResource.java
@@ -1,13 +1,26 @@
 package ch.mabaka.journal.app;
 
+import javax.inject.Inject;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import org.jboss.resteasy.annotations.jaxrs.PathParam;
 
 @Path("/hello")
 public class GreetingResource {
 
+	@Inject
+	private GreetingService greetingService;
+	
+	@GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/{name}")
+    public String greeting(@PathParam String name) {
+        return greetingService.greeting(name);
+    }
+	
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
---
+++ src/main/java/ch/mabaka/journal/app/GreetingService.java
@@ -0,0 +1,12 @@
+package ch.mabaka.journal.app;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class GreetingService {
+
+    public String greeting(String name) {
+        return "hello " + name;
+    }
+
+}
